3528f9353a5eebf50c3abf07495bb052836b4c2d
beetrbgus <devbeetrb0214@gmail.com>
2021-12-24 18:20:59 +0900

    DTO 추가

---
+++ onemile/src/main/java/com/kh/onemile/dto/member/town/TownLocationDTO.java
@@ -0,0 +1,12 @@
+package com.kh.onemile.dto.member.town;
+
+import lombok.Data;
+
+// 내 동네 위치 DTO
+@Data
+public class TownLocationDTO {
+	private int tlNo;
+	private int townNo;
+	private String lat;
+	private String lon;
+}
--- onemile/src/main/java/com/kh/onemile/dto/miles/board/MilesBoardDTO.java
+++ onemile/src/main/java/com/kh/onemile/dto/miles/board/MilesBoardDTO.java
@@ -9,7 +9,6 @@ public class MilesBoardDTO {
 	private int mbNo;
 	private int milesNo;
 	private int memberNo;
-	private int mcNo; //게시판 카테고리 번호( 공지 , 후기 ,자유 )
 	
 	private String title;
 	private String context;
