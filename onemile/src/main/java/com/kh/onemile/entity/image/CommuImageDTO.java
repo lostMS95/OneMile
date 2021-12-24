680e36604a2dcb2ede86f1efee4a4f53f55705cb
ChoeByeol <cb68862643@gmail.com>
2021-12-24 21:17:33 +0900

    DTO 추가

---
+++ onemile/src/main/java/com/kh/onemile/entity/image/CommuImageDTO.java
@@ -0,0 +1,10 @@
+package com.kh.onemile.entity.image;
+
+import lombok.Data;
+
+@Data
+public class CommuImageDTO {
+	private int cmiNo;
+	private int imageNo;
+	private int commuNo;
+}
---
+++ onemile/src/main/java/com/kh/onemile/entity/member/MemberTemperatureDTO.java
@@ -0,0 +1,7 @@
+package com.kh.onemile.entity.member;
+
+public class MemberTemperatureDTO {
+	private int tpNo;
+	private int memberNo;
+	private int tp;
+}
