1c6faba8434f5b4aa7da6d7668d49b20aea14fdc
beetrbgus <devbeetrb0214@gmail.com>
2021-12-24 20:44:14 +0900

    Member 가입 구문 Mapper는 안함.

--- onemile/src/main/java/com/kh/onemile/controller/MemberController.java
+++ onemile/src/main/java/com/kh/onemile/controller/MemberController.java
@@ -1,10 +1,28 @@
 package com.kh.onemile.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.onemile.dto.member.MemberDTO;
+import com.kh.onemile.service.member.MemberService;
+
 
 @RequestMapping("/member")
 @Controller
 public class MemberController {
+	@Autowired
+	private MemberService memberService; 
 	
+	@GetMapping("/join")
+	public String getJoin() {
+		return "join";
+	}
+	@PostMapping("/join")
+	public void postJoin(@ModelAttribute MemberDTO memberDTO) {
+		memberService.join(memberDTO);
+	}
 }
