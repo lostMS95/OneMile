fce06be8bc6d662bbcf8eb58264234f5fbd0a3c0
ChoeByeol <cb68862643@gmail.com>
2021-12-27 11:35:04 +0900

    이메일 서비스 수정

--- onemile/src/main/java/com/kh/onemile/service/email/GmailService.java
+++ onemile/src/main/java/com/kh/onemile/service/email/GmailService.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.kh.onemile.entity.member.MemberDTO;
 import com.kh.onemile.entity.member.certi.CertiDTO;
+import com.kh.onemile.repository.CertiDao;
+import com.kh.onemile.repository.MemberDao;
 import com.kh.onemile.util.RandomUtil;
 
+@Transactional //0 or 100? 
 @Service
 public class GmailService implements EmailService{
 	
@@ -17,27 +20,37 @@ public class GmailService implements EmailService{
 	
 	@Autowired
 	private RandomUtil randomUtil;
+	
+	@Autowired
+	private CertiDao certiDao;
 
+	@Autowired
+	private MemberDao memberDao;
+	
 	@Override
-	public void sendCertificationNumber(MemberDTO memberDTO,String email) {
+	public void sendCertificationNumber(String email) {
 		//랜덤번호 생성 부분
 		String number = randomUtil.RandomNumber(6);
 		
+		int memberNo = memberDao.getMemberNo(email);
+		System.out.println("찾기"+memberNo);
+		
 		//실제 이메일 발송 부분
 		SimpleMailMessage message = new SimpleMailMessage();
-		message.setTo(memberDTO.getEmail());
+		message.setTo(email);
 		message.setSubject("[원마일] 인증번호 메일입니다");
 		message.setText("인증번호 : " + number);
 		sender.send(message);
 		
+		
 		//데이터베이스 등록 부분
 		CertiDTO certiDTO = new CertiDTO();
-		certiDTO.setEmail(memberDTO.getEmail());
+		certiDTO.setEmail(email);
 		certiDTO.setSerial(number);
-	
+		certiDTO.setMemberNo(memberNo);
+
 		
-	
-		
+		certiDao.insert(certiDTO);
 		
 	}
 }
