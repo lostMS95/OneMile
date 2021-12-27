a98d3ff53b65ff99b867c578487cc07f63a98f16
lostMS95 <rmsdn0415@naver.com>
2021-12-27 16:07:07 +0900

    패키지 정리 및 코드 정리, 주석 추가

--- onemile/src/main/java/com/kh/onemile/repository/admin/AdminDaoImpl.java
+++ onemile/src/main/java/com/kh/onemile/repository/admin/AdminDaoImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kh.onemile.entity.admin.ApproveDTO;
-import com.kh.onemile.vo.MemberJoinVO;
 
 @Repository
 public class AdminDaoImpl implements AdminDao{
--- onemile/src/main/java/com/kh/onemile/service/admin/AdminService.java
+++ onemile/src/main/java/com/kh/onemile/service/admin/AdminService.java
@@ -1,7 +1,5 @@
 package com.kh.onemile.service.admin;
 
-import com.kh.onemile.entity.admin.ApproveDTO;
-
 public interface AdminService {
 	final String SEQNAME="approve_seq";
 	void regApproveMember(int memberNo);
