a88b2e7ddf1f2f59ac3cf6294800fc9fb48b65ea
beetrbgus <devbeetrb0214@gmail.com>
2021-12-28 14:51:41 +0900

    프론트에서 지도 위치 인증

--- onemile/src/main/webapp/resources/js/navigator.js
+++ onemile/src/main/webapp/resources/js/navigator.js
@@ -1,5 +1,26 @@
-if('geolocation' in navigator){
-    navigator.geolocation.getCurrentPosition((position) => {
-        alert(position.coords.latitude +""+ position.coords.longitude);
-    });
-}
+$(function(){
+	$("#confirmbtn").click(function(){
+		if('geolocation' in navigator){
+		    navigator.geolocation.getCurrentPosition((position) => {
+		    	
+		    	let lat = position.coords.latitude;
+		    	let lng = position.coords.longitude;
+		    	
+		    	getAddr(lat,lng);
+		    });
+
+		}
+	});
+	function getAddr(lat,lng){
+		let geocoder = new kakao.maps.services.Geocoder();
+
+	    let coord = new kakao.maps.LatLng(lat, lng);
+	    let callback = function(result, status) {
+	        if (status === kakao.maps.services.Status.OK) {
+	            console.log(result);
+	        }
+	        
+	    };
+	    geocoder.coord2RegionCode(lng,lat,callback);
+	}
+});
